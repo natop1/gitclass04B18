25ff0b497ea158c80a5a4c6c2f970edd45340a21
natop1 <natotsu3@gmail.com>
2024-02-15 23:51:29 -0500

    ewrwer

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println(" i am bach18 student");
+        System.out.println("new commit by new user");
     }
 }
